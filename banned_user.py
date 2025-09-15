a746ca8ed7a3de62ac9d50c477423bd880f18bdf
root <root@goldfish.localdomain>
2025-09-15 17:03:34 +0800

    5.2

---
+++ banned_user.py
@@ -0,0 +1,12 @@
+banned_users = ['andrew','carolina','david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+
+car = 'subaru'
+print("\nIs car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
