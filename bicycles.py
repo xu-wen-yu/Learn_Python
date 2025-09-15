88c06c47c2bd8eceeffc374b08c0a48c3d04b89f
root <root@goldfish.localdomain>
2025-09-15 15:44:02 +0800

    motorcycles

---
+++ bicycles.py
@@ -0,0 +1,7 @@
+bicycles = ['trek','cannondale','redline','speciallized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[-1])
+print(bicycles[-2])
+message = f"My first bycicle was a {bicycles[0].title()}."
+print(message)
---
+++ motorcycles.py
@@ -0,0 +1,38 @@
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+motorcycles.insert(0,'ducati')
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+motorcycles.remove('yamaha')
+print(motorcycles)
+
+motorcycles = ['honda','yamaha','suzuki','ducati']
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+motorcycles = ['yamaha','yamaha','yamaha','yamaha']
+motorcycles.remove('yamaha')
+print(motorcycles)
