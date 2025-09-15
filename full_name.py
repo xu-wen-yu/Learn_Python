c6687354bda987135e5d3cd7b92c0e244b87cc20
root <root@goldfish.localdomain>
2025-09-15 15:08:00 +0800

    full_name.py

---
+++ full_name.py
@@ -0,0 +1,17 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("\tPython")
+print("Language:\nPython\nC\nJavaScript")
+print("Language:\n\tPython\n\tC\n\tJavaScript")
+
+
+favorite_language = "python"
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
