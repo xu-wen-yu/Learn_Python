f285c652da9021726ea2fdab3c03b86ccf08abf4
root <root@goldfish.localdomain>
2025-10-11 23:26:54 +0800

    Unit6 Finished

---
+++ favorite_language.py
@@ -0,0 +1,32 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+""" language = favorite_languages['jen'].title()
+print(f"Jen's favorite language is {language}.") """
+""" 
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.") """
+
+""" for name in favorite_languages.keys():
+    print(name.title()) """
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
